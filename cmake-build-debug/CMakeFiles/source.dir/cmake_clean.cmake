1cddcc319fbee52db28d9761d37696f7ee042773
yurygavrilov47 <yurygavrilov47@gmail.com>
2019-12-13 12:30:17 +0300

    first

---
+++ cmake-build-debug/CMakeFiles/source.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/source.dir/sources/source.cpp.o"
+  "libsource.dll.a"
+  "source.exe"
+  "source.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/user/Desktop/qq/laba1-master
+# Build directory: /cygdrive/c/Users/user/Desktop/qq/laba1-master/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/cygdrive/c/Users/user/Desktop/qq/laba1-master/cmake-build-debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/c/Users/user/Desktop/qq/laba1-master/CMakeLists.txt;58;add_test;/cygdrive/c/Users/user/Desktop/qq/laba1-master/CMakeLists.txt;0;")
